2317fa640cf048eb5ce26e0a234f9f1a0fb5f934
sajedulislamsaju <sajedulislamsaju123@gmail.com>
2023-08-12 13:28:05 -0700

    some changes in the project

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -2,7 +2,7 @@
 const Main = () => {
     return (
         <div>
-            
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore repudiandae placeat cum modi cumque delectus, neque ipsum commodi nobis incidunt ipsam beatae sit dicta, architecto quaerat recusandae ex ea dolorum.
         </div>
     );
 };
